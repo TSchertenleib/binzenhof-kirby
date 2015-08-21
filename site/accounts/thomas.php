0d82834a113a2e02e04790bb8a8e0391bfdf7a8d
Thomas Schertenleib <thomas.schertenleib@gmail.com>
2015-08-21 20:28:17 +0200

    aufgesetzt

---
+++ site/accounts/thomas.php
@@ -0,0 +1,14 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: thomas
+email: thomas.schertenleib@gmail.com
+password: >
+  $2a$10$Lve1S/pUrSl.D.b7BgCOmerFGEH2XFT7hB94XuuOBbv0PpWiSTnVq
+language: de
+role: admin
+history:
+  - hoflaedeli
+  - kontakt
+  - events
+  - unser-bauernhof
+  - home
---
+++ site/templates/home.php
@@ -0,0 +1,7 @@
+<?php snippet('header') ?>
+
+
+    <?php snippet('home') ?>
+
+
+<?php snippet('footer') ?>
